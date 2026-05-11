988f5a5fecae9198954018f5f5c7fb974d545262
MiGoYAm <67077321+MiGoYAm@users.noreply.github.com>
2026-05-11 17:09:29 +0200

    Initial

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Suspense } from 'react'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </StrictMode>,
+)
